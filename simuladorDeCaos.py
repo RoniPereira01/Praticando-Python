5e275d9b40f4a552a502cf5271c1b1507c4e3f46
Ronilson <ronilsonpereira.pro@gmail.com>
2024-12-23 19:52:08 -0300

    Continuação das Classes, metodos

--- simuladorDeCaos.py
+++ simuladorDeCaos.py
@@ -2,15 +2,18 @@ import random
 
 class Academia:
     def __ini__(self):
-        self.halteres = [i for i in range(10, 36) if i % 2 ==0]
+        self.halteres = [i for i in range(10, 100) if i % 2 ==0]
         self.porta_halteres = {}
         self.reniciar_o_dia()
 
     def reniciar_o_dia(self):
         self.porta_halteres = {i: i for i in self.halteres}
-
+    
     def listar_halteres(self):
         return (i for i in self.porta_halteres.values() if i != 0)
+
+    def listar_halteres(self):
+        return (i for i, j in self.porta_halteres.items() if j == 0)
     
     def pegar_halteres(self, peso):
         halt_pos = list(self.porta_halteres.values()).index(peso)
@@ -25,7 +28,51 @@ class Academia:
         return len(num_caos) / len(self.porta_halteres)
 
 
+class Usuario:
+    def __init__(self, tipo, academia):
+        self.tipo = tipo
+        self.academia = academia
+        self.peso = 0
+    
+    def iniciar_treino(self):
+        lista_pesos = self.academia(lista_pesos)
+        self.peso = random.choice(lista_pesos)
+        self.academia.pegar_halteres(self.peso)
+
+    def finalizar_treino(self):
+        espacos = self.academia.listar_espacos()
+
+        if self.tipo == 1:
+            if self.peso in espacos:
+                self.academia.devolver_halter(self.peso, self.peso)
+            else:
+                pos = random.choice(espacos)
+                self.academia.devolver_halter(pos, self.peso)
+        
+        if self.tipo == 2:
+            pos = random.choice(espacos)
+            self.academia.devolver_halter(pos,self.peso)
+        self.peso = 0 
+
+academia = Academia()
+usuarios = [Usuario(1, academia) for i in range(10)]
+usuarios += [Usuario(1, academia) for i in range(1)]
+random.shuffle(usuarios)
+
+
+for k in range(10):
+    academia.reiniciar_o_dia()
+    for i in range(10):
+        random.shuffle(usuarios)
+        for user in usuarios:
+            user.iniciar_treino()
+        for user in usuarios:
+            user.finalizar_treino()
+    list_chaos += [academia.calcular_caos()] # type: ignore
 
 
 
-self = Academia()
+import seaborn as sns
+sns.displot(list_chaos) # type: ignore
+
+academia.porta_halteres()
